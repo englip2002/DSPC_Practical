e5855ec4ccba003bca898b13f9cc4b67b29c15f6
TARUMT <TARUMT@PC-5>
2023-07-24 08:05:24 +0800

    p4

--- DistributedSystemAndParallelComputingPractical/P3Q1.cpp
+++ DistributedSystemAndParallelComputingPractical/P3Q1.cpp
@@ -2,33 +2,39 @@
 #include "stdio.h"
 #include "stdlib.h"
 
-int main()
-{
+void a() {
 	//parallel region - only this part will run parallel
-//#pragma omp parallel
-//	{
-//		int ID = omp_get_thread_num();
-//		printf("hello(%d)", ID);
-//		printf("world(%d)\n", ID);
-//	}
+#pragma omp parallel
+	{
+		int ID = omp_get_thread_num();
+		printf("hello(%d)", ID);
+		printf("world(%d)\n", ID);
+	}
+}
 
+
+void b() {
 	//share data
-//	int x = 5;
-//#pragma omp parallel
-//	{
-// 	    //share memory - result not same
-//		x = x + 1;
-//		printf("shared: X is %d\n", x);
-//	};
+	int x = 5;
+#pragma omp parallel
+	{
+		//share memory - result not same
+		x = x + 1;
+		printf("shared: X is %d\n", x);
+	};
+}
 
+void c() {
 	//private data
-//#pragma omp parallel
-//	{
-//		//reinitialized - result is same
-//		int x; x = 3;
-//		printf("private: X is %d\n", x);
-//	}
+#pragma omp parallel
+	{
+		//reinitialized - result is same
+		int x; x = 3;
+		printf("private: X is %d\n", x);
+	}
+}
 
+void d() {
 	int x = 5;
 #pragma omp parallel
 	{
@@ -37,4 +43,12 @@ int main()
 	}
 	printf("after: x is %d\n", x); // also dangerous
 	return 0;
+}
+
+int main()
+{
+	a();
+	b();
+	c();
+	d();
 }
